95ef89f231c7dac095b426a8af1ecadd5d2365f3
Edward Njoroge <codingedward@gmail.com>
2018-07-03 02:37:28 +0300

    Add axios and axios middleware for requests handling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+    createStore,
+    applyMiddleware
+} from 'redux';
+import {
+    Provider
+} from 'react-redux';
+import mainReducer from './reducers';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = axios.create({
+    baseURL: 'http://localhost:5000/api/v1',
+    responseType: 'json',
+});
+
+const store = createStore(mainReducer,
+    applyMiddleware(axiosMiddleware(client)));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
