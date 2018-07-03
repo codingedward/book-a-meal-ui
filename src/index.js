9710a04cc7d77cd1715ce0b573b0a8325d1de67e
Edward Njoroge <codingedward@gmail.com>
2018-07-04 02:41:21 +0300

    Add router middleware for auto redirection

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {
-    createStore,
-    applyMiddleware
-} from 'redux';
-import {
-    Provider
-} from 'react-redux';
-import mainReducer from './reducers';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import axios from 'axios';
 import axiosMiddleware from 'redux-axios-middleware';
+
+import { createBrowserHistory } from 'history';
+import { 
+    ConnectedRouter,
+    connectRouter, 
+    routerMiddleware 
+} from 'connected-react-router';
+
+import App from './App';
+import appReducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
+const history = createBrowserHistory();
 
 const client = axios.create({
     baseURL: 'http://localhost:5000/api/v1',
     responseType: 'json',
 });
 
-const store = createStore(mainReducer,
-    applyMiddleware(axiosMiddleware(client)));
-
+const store = createStore(
+    connectRouter(history)(appReducer),
+    applyMiddleware(axiosMiddleware(client), routerMiddleware(history))
+);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
 );
