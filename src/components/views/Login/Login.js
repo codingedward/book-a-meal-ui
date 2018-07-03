42f7444696600868beedc742fc2255e6e0358375
Edward Njoroge <codingedward@gmail.com>
2018-07-04 02:38:05 +0300

    Update login page component with AuthPage

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,61 +1,70 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Alert } from 'reactstrap';
+import { Redirect, Link } from 'react-router-dom';
+
+import AuthPage from '../../common/AuthPage';
 import './styles.css';
 
 class Login extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {email: '', password: ''};
-        this.handleInput = this.handleInput.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    state = {
+        email: '',
+        password: '',
     }
 
-    handleSubmit(evt) {
-        evt.preventDefault()
-        this.props.authenticate(this.state)
+    onSubmit = (evt) => {
+        evt.preventDefault();
+        this.props.login(this.state);
     }
 
-    handleInput(evt) {
-        let data = {...this.state};
-        data[evt.target.name] = evt.target.value;
-        this.setState(data);
+    onChange = (evt) => {
+        this.setState({
+            ...this.state,
+            [evt.target.name]: evt.target.value
+        })
     }
 
     render() {
+        const { payload, error, loading } = this.props.response;
+        if (payload && payload.data.access_token) {
+            return <Redirect to="/home" />
+        }
+
         return (
-            <main className="patterns bg-teal">
-                <section className="container login">
-                    <div className="row pt-5">
-                        <div className="col-12 text-center">
-                            <h1 className="logo-small text-flav">BAM!</h1>
-                        </div>
-                    </div>
-                    <div className="row mb-5 pl-3 pr-3 p-sm-0">
-                        <form 
-                            className="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 card"
-                            onSubmit={this.handleSubmit}>
-                            <h5 className="text-center mb-3 mt-4">Login</h5>
-                            <h6 className="text-center">{this.props.auth}</h6>
-                            <label>Email </label>
-                            <input 
-                                type="email" 
-                                className="form-control" 
-                                name="email" 
-                                onChange={this.handleInput} />
-                            <label>Password </label>
-                            <input 
-                                type="password" 
-                                className="form-control" 
-                                name="password" 
-                                onChange={this.handleInput} />
-                            <button className="btn btn-primary mt-2">Continue </button>
-                            <p className="text-center pt-3"> <a className="" href="sign-up.html">Create Account?</a></p>
-                        </form>
-                    </div>
-                </section>
-            </main>
+            <AuthPage>
+                <form 
+                    className="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4 card"
+                    onSubmit={this.onSubmit}>
+
+                    <h5 className="text-center mb-3 mt-4">Login</h5>
+                    {error && <Alert color="danger" > {error} </Alert> }
+                    <label>Email </label>
+                    <input 
+                        type="email" 
+                        className="form-control" 
+                        name="email" 
+                        onChange={this.onChange} />
+                    <label>Password </label>
+                    <input 
+                        type="password" 
+                        className="form-control" 
+                        name="password" 
+                        onChange={this.onChange} />
+                    <Button disabled={loading} className="btn btn-primary mt-2">Continue </Button>
+                    <p className="text-center pt-3"> 
+                        <Link to="/sign-up"> Create Account? </Link>
+                    </p>
+                </form>
+            </AuthPage>
         );
     }
+}
+
+Login.propTypes = {
+    response: PropTypes.object.isRequired,
+    redirect: PropTypes.func.isRequired,
+    login: PropTypes.func.isRequired
 }
 
 export default Login;
