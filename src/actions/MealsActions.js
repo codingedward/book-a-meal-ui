c45ee021faf6ccfc8b57a023b143d4f907b02061
Edward Njoroge <codingedward@gmail.com>
2018-07-08 21:33:33 +0300

    Add edit meal actions

--- src/actions/MealsActions.js
+++ src/actions/MealsActions.js
@@ -1,15 +1,104 @@
-export const GET_MEALS = 'GET_MEALS';
+import axios from '../axios';
+import { IMAGES_UPLOAD_URL } from '../constants';
 
+export const CREATE_MEAL = 'ADD_MEAL';
+export const FETCH_MEALS = 'FETCH_MEALS';
+export const FETCH_COUNT = 10;
+export const EDIT_MEAL = 'EDIT_MEAL';
 
-export function getMealsAct() {
+export function createMeal(meal, meta) {
+
+    if (meal.image) {
+        let image = {
+            file: meal.image,
+            upload_preset: 'pztk1zzn'
+        }
+        // async, first post image
+        const response = axios.post(IMAGES_UPLOAD_URL, image);
+        return (dispatch) => {
+            response.then(({ data }) => {
+                 dispatch({
+                    type: CREATE_MEAL,
+                    payload: {
+                        request: {
+                            method: 'post',
+                            url: '/meals',
+                            data: {
+                                name: meal.name,
+                                cost: meal.cost,
+                                img_url: data.secure_url,
+                            }
+                        }
+                    }
+                });
+            })
+        }
+    } 
+
     return {
-        type: GET_MEALS,
+        type: CREATE_MEAL,
+        payload: {
+            request: {
+                method: 'post',
+                url: '/meals',
+                data: {
+                    name: meal.name,
+                    cost: meal.cost,
+                }
+            }
+        }
+    };
+}
+
+export function editMeal(meal) {
+    if (meal.image) {
+        let image = {
+            file: meal.image,
+            upload_preset: 'pztk1zzn'
+        }
+        const response = axios.post(IMAGES_UPLOAD_URL, image);
+        return (dispatch) => {
+            response.then(({ data }) => {
+                 dispatch({
+                    type: EDIT_MEAL,
+                    payload: {
+                        request: {
+                            method: 'put',
+                            url: `/meals/${meal.id}`,
+                            data: {
+                                name: meal.name,
+                                cost: meal.cost,
+                                img_url: data.secure_url,
+                            }
+                        }
+                    }
+                });
+            })
+        }
+    } 
+
+    return {
+        type: EDIT_MEAL,
+        payload: {
+            request: {
+                method: 'put',
+                url: `/meals/${meal.id}`,
+                data: meal
+            }
+        }
+    };
+
+}
+
+
+export function fetchMeals(page = 1, per_page = FETCH_COUNT) {
+    return {
+        type: FETCH_MEALS,
         payload: {
             request: {
                 method: 'get',
-                url: '/meals',
+                url: `/meals?per_page=${per_page}&page=${page}`,
             }
         }
     }
 }
-
