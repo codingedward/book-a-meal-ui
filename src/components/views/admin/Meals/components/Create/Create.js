5efd638d0c330bdb2c6a4cba05f588da375ae0aa
Edward Njoroge <codingedward@gmail.com>
2018-07-08 21:40:37 +0300

    Update create meal modal to post meal details

--- src/components/views/admin/Meals/components/Create/Create.js
+++ src/components/views/admin/Meals/components/Create/Create.js
@@ -1,27 +1,73 @@
 import React from 'react';
 import Modal from '../../../../../common/Modal';
 import ImageInput from '../../../../../common/ImageInput';
+import { Alert, Input, Button } from 'reactstrap';
+import { Status } from '../../../../../../constants';
+import { singleError } from '../../../../../../utils';
 
 import './styles.css';
 
-const CreateModal = ({ isOpen, onCreate, toggle }) => {
-    const body = (
-        <div>
-            <ImageInput />
-            <label> Name </label>
-            <input type="text" />
-            <label> Cost </label>
-            <input type="text" />
-        </div>
-    );
 
-    const footer = (
-        <button color="primary" className="m-auto" onClick={() => onCreate()}>Save Meal</button>
-    )
+class CreateModal extends React.Component {
 
-    return (
-        <Modal title="Add New Meal" body={body} footer={footer}/>
-    );
+    onImageChange = (data) => {
+        this.setState({
+            ...this.state,
+            image: data,
+        });
+    }
+
+    onChange = (e) => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSave = () => {
+        this.props.createMeal({
+            ...this.state,
+        });
+    }
+
+    render() {
+        const { error, createStatus } = this.props.meals;
+        const body = (
+            <div>
+                {createStatus === Status.SUCCESS &&
+                        <Alert className="text-center text-small" color="success">
+                            Successfully added
+                        </Alert>
+                }
+                {createStatus === Status.FAIL &&
+                        <Alert className="text-center text-small" color="danger">
+                            { singleError(error) }
+                        </Alert>
+                }
+
+                <ImageInput onChange={(data) => this.onImageChange(data)} />
+                <label> Name </label>
+                <Input name="name" onChange={this.onChange} type="text" />
+                <label> Cost </label>
+                <Input name="cost" onChange={this.onChange} type="number" />
+            </div>
+        );
+
+        const footer = (
+            <Button color="primary" className="m-auto"onClick={() => this.onSave()}>Save Meal</Button>
+        )
+        const { isOpen, toggle } = this.props;
+
+        return (
+            <Modal 
+                title="Add New Meal" 
+                body={body} 
+                footer={footer} 
+                isOpen={isOpen}
+                toggle={toggle}
+            />
+        );
+    }
 }
 
 export default CreateModal;
