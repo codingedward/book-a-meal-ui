0dc46c684ce82f8ec17c6f1d2179e15bc31dbe00
Edward Njoroge <codingedward@gmail.com>
2018-07-08 21:54:09 +0300

    Add edit meals reducer

--- src/reducers/MealsReducer.js
+++ src/reducers/MealsReducer.js
@@ -1,11 +1,96 @@
-import reducerFactory from './reducerFactory';
-import { GET_MEALS } from '../actions/MealsActions';
+import { Status } from '../constants';
+import { 
+    EDIT_MEAL,
+    CREATE_MEAL, 
+    FETCH_MEALS,
+    FETCH_COUNT,
+} from '../actions/MealsActions';
 
-const meals = reducerFactory({
-    actionName: GET_MEALS,
-    initialState: [],
-    stateField: 'meals'
-});
 
-export default meals;
+const initialState = {
+    payload: {
+        meals:[]
+    }
+}
+
+const mealsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_MEALS:
+            return { 
+                payload: { ...state.payload },
+                fetchStatus: Status.STARTED,
+            };
+
+        case `${FETCH_MEALS}_SUCCESS`:
+            return {
+                payload: action.payload.data,
+                fetchStatus: Status.SUCCESS
+            };
+
+        case `${FETCH_MEALS}_FAIL`:
+            return {
+                payload: { ...state.payload },
+                error: action.error.response,
+                fetchStatus: Status.FAIL
+            };
+
+        case CREATE_MEAL:
+            return { 
+                payload: { ...state.payload },
+                createStatus: Status.STARTED
+            }
+
+        case `${CREATE_MEAL}_SUCCESS`:
+            let meals = [action.payload.data.meal].concat(state.payload.meals);
+            meals.concat(state.payload.meals);
+
+            // ensure maximum meals count is FETCH_COUNT
+            while (meals.length > FETCH_COUNT) {
+                meals.pop();
+            }
+            return {
+                payload: { 
+                    ...state.payload,  
+                    meals,
+                },
+                createStatus: Status.SUCCESS,
+            }
+
+        case `${CREATE_MEAL}_FAIL`:
+            return {
+                payload: { ...state.payload },
+                error: action.error.response,
+                createStatus: Status.FAIL
+            }
+
+        case EDIT_MEAL:
+            return {
+                payload: { ...state.payload },
+                editStatus: Status.STARTED
+            }
+        case `${EDIT_MEAL}_SUCCESS`:
+            const { meal } = action.payload.data
+            return {
+                payload: {
+                    meals: state.payload.meals.map(currentMeal => {
+                        if (currentMeal.id === meal.id) {
+                            return meal;
+                        }
+                        return currentMeal;
+                    })
+                },
+                editStatus: Status.SUCCESS
+            }
+        case `${EDIT_MEAL}_FAIL`:
+            return {
+                payload: { ...state.payload },
+                error: action.error.response,
+                editStatus: Status.FAIL
+            }
+        default:
+            return state;
+    }
+}
+
+export default mealsReducer;
 
