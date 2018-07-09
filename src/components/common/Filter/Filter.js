ab2045bf21fd90285d3f22fafb7a26f7a19d4d87
Edward Njoroge <codingedward@gmail.com>
2018-07-10 02:35:00 +0300

    Filter should be automatic

--- src/components/common/Filter/Filter.js
+++ src/components/common/Filter/Filter.js
@@ -1,21 +1,54 @@
 import React from 'react';
+import './styles.css';
 
+class Filter extends React.Component {
 
-const Filter = ({ onFilter }) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+        };
+        this.inputRef = React.createRef();
+    }
 
-    return (
-        <div className="row mb-3">
-            <div className="col-3 col-sm-2 col-lg-1 text-right">
-                <label className="pt-2">Text:</label>
+    onReset = (e) => {
+        this.setState({
+            ...this.state,
+            text: '',
+        });
+        this.inputRef.current.value = '';
+        this.props.onFilter('')
+    }
+
+    onChange = (e) => {
+        this.setState({
+            ...this.state,
+            text: e.target.value
+        });
+        this.props.onFilter(e.target.value)
+    }
+
+    render() {
+        return (
+            <div className="row mb-3">
+                <div className="col-3 col-sm-2 col-lg-1 text-right">
+                    <label className="pt-2">Text:</label>
+                </div>
+                <div className="col-9 col-sm-7 col-lg-4 col-xl-3 pl-0">
+                    <input 
+                        ref={this.inputRef} 
+                        onChange={this.onChange} 
+                        type="text" 
+                        className="d-inline form-control ml-0 mb-0"  placeholder="Type to filter..."/>
+                </div>
+                {this.state.text &&
+                    <div className="animated fadeIn btn-reset col-12 col-sm-1 col-lg-1 pt-3 pt-sm-0">
+                        <button onClick={this.onReset} className="btn btn-secondary d-block m-auto">Reset </button>
+                    </div>
+                }
             </div>
-            <div className="col-9 col-sm-7 col-lg-4 col-xl-3 pl-0">
-               <input type="text" className="d-inline form-control ml-0 mb-0"  placeholder=" Search text..."/>
-            </div>
-            <div className="col-12 col-sm-1 col-lg-1 pt-3 pt-sm-0">
-                <button onClick={() => onFilter()} className="btn btn-secondary d-block m-auto">Filter </button>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Filter;
