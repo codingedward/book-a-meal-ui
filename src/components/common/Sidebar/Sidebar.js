a24c7ed75e37522f83671824ec40943ce43bd7cb
Edward Njoroge <codingedward@gmail.com>
2018-07-10 22:08:35 +0300

    Refactor sidebar to receive children props

--- src/components/common/Sidebar/Sidebar.js
+++ src/components/common/Sidebar/Sidebar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './styles.css';
 
-const Sidebar = ({ children, logout }) => (
-    <nav className="d-none d-md-block col-md-4 col-lg-3 controls sidebar patterns bg-teal text-center"> 
-        <h1 className="logo-medium text-flav pt-3">BAM!</h1> 
-        <hr/>
-        {children}
-        <hr className="last"/>
-        <button onClick={() => logout()} className="btn btn-logout">Logout</button>
-    </nav>
-);
+class Sidebar extends React.Component {
+
+    logout = () => {
+        localStorage.clear();
+        this.props.history.push('/');
+    }
+
+    render() {
+        return (
+            <nav className="d-none d-md-block col-md-4 col-lg-3 controls sidebar patterns bg-teal text-center"> 
+                <h1 className="logo-medium text-flav pt-3">BAM!</h1> 
+                <hr/>
+                {this.props.children}
+                <hr className="last"/>
+                <button onClick={this.logout} className="btn btn-logout">Logout</button>
+            </nav>
+        );
+    };
+}
 
 export default Sidebar;
