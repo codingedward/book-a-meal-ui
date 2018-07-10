be23d2fe2eb9cadc17afe06d5c4e00b8478e80f1
Edward Njoroge <codingedward@gmail.com>
2018-07-10 23:17:15 +0300

    Implement double routing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/views/Landing';
 import Login from 'src/components/views/auth/Login';
 import Register from 'src/components/views/auth/Register';
-import Meals from 'src/components/views/admin/Meals';
+import Admin from 'src/components/views/admin';
 import './styles.css';
 
 class App extends React.Component {
@@ -13,8 +13,8 @@ class App extends React.Component {
             <Router>
                 <Switch>
                     <Route exact path="/" component={Landing} />
+                    <Route path="/admin" component={Admin} />
                     <Route path="/login" component={Login} />
-                    <Route path="/meals" component={Meals} />
                     <Route path="/sign-up" component={Register} />
                 </Switch>
             </Router>
