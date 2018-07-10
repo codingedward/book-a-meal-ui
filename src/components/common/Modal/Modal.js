3ac20fbc826b985e448b85a986a22673d71861aa
Edward Njoroge <codingedward@gmail.com>
2018-07-10 22:06:44 +0300

    Add on opened method

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -7,8 +7,8 @@ import {
 } from 'reactstrap';
 import './styles.css';
 
-const Modal = ({ title, body, footer, isOpen, toggle }) => (
-    <BModal isOpen={isOpen} toggle={toggle} fade={false}>
+const Modal = ({ onOpened, title, body, footer, isOpen, toggle }) => (
+    <BModal isOpen={isOpen} onOpened={onOpened} toggle={toggle} fade={false}>
         <ModalHeader toggle={toggle}>
             {title}
             <button onClick={() => toggle()} type="button" className="close">
