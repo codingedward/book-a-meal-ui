7a896e604059c99a4a6b57cc37abb5c86c8904c7
Edward Njoroge <codingedward@gmail.com>
2018-07-11 03:18:35 +0300

    Add test data for table component

--- src/components/common/Table/Table.test.js
+++ src/components/common/Table/Table.test.js
@@ -3,10 +3,22 @@ import { shallow }  from 'enzyme';
 import 'src/utils/bootTests'; // order matters!
 import Table from './Table';
 import Adapter from 'enzyme-adapter-react-16';
+import { EntryType } from 'src/constants';
+
+const data = {
+    columns: [
+        {
+            type: EntryType.TEXT,
+            key: 'name',
+            title: 'Name',
+        }
+    ],
+    rows: [ { name: 'John Doe' } ]
+}
 
 describe('<Table />', () => {
     it('renders correctly', () => {
-        const wrapper = shallow(<Table />);
+        const wrapper = shallow(<Table data={data} />);
         expect(wrapper).toMatchSnapshot();
     });
 });
