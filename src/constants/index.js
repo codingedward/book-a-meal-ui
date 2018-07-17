1efaa29e88834c2b4f84e8a63ebb50f4d84a6ee5
Edward Njoroge <codingedward@gmail.com>
2018-07-17 22:03:57 +0300

    Fix base url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8000/api/v1';
+export const BASE_URL = 'https://andela-bam-api.herokuapp.com/api/v1';
 export const IMAGE_UPLOAD_PRESET = 'pztk1zzn';
 export const IMAGES_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/codingedward/image/upload';
 
