005813279c19e6a6dca40afe6ff4a1827fc0d68e
Edward Njoroge <codingedward@gmail.com>
2018-07-17 03:38:07 +0300

    Fix menu item to recieve the menu

--- src/components/common/Menu/Menu.js
+++ src/components/common/Menu/Menu.js
@@ -2,11 +2,11 @@ import React from 'react';
 import MenuItem from './components/MenuItem';
 import './styles.css';
 
-const Menu = ({ items, name }) => (
+const Menu = ({ items, menu = {}, onOrder }) => (
     <div>
-        <h5>{name}</h5>
+        <h5 className="text-center mt-2">{menu.name}</h5>
         <div className="row">
-            {items.map(item => <MenuItem item={item} key={item.id} />)}
+            {items.map(item => <MenuItem menu={menu} item={item} key={item.id} onOrder={onOrder} />)}
         </div>
     </div>
 );
--- src/components/common/Menu/components/MenuItem/MenuItem.js
+++ src/components/common/Menu/components/MenuItem/MenuItem.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './styles.css';
+import placeholder from './assets/placeholder.png';
 
-const MenuItem = ({ item }) => (
-    <div className="col-10 offset-1 offset-sm-0 col-sm-6 col-md-4 col-lg-3 text-center">
-        <div className="menu-item pt-3 pl-3 pr-3 m-1">
-            <img src={item.img_url} className="img-fluid" alt="Food"/>
-            <table className="table">
-                <tbody>
-                    <tr>
-                        <td className="p-2">Ksh. {item.cost}</td>
-                    </tr>
-                    <tr>
-                        <td className="p-2">{item.name}</td>
-                    </tr>
-                    <tr>
-                        <td className="pb-0">
-                            <Link to="/login" className="btn btn-primary"> Order Now</Link>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+const MenuItem = ({  menu, item, onOrder }) => {
+
+    let img_url = item.img_url;
+    if (! img_url || img_url === '#')
+        img_url = placeholder;
+    const { meal = {} } = item;
+    return (
+        <div className="col-10 offset-1 offset-sm-0 col-sm-6 col-md-4 col-lg-3 text-center">
+            <div className="menu-item pt-3 pl-3 pr-3 m-1">
+                <img src={img_url} className="img-fluid" alt="Food"/>
+                <table className="table">
+                    <tbody>
+                        <tr>
+                            <td className="p-2">Ksh. {meal.cost}</td>
+                        </tr>
+                        <tr>
+                            <td className="p-2">{meal.name}</td>
+                        </tr>
+                        <tr>
+                            <td className="pb-0">
+                                <button 
+                                    className="btn btn-primary" 
+                                    onClick={() => onOrder(item, menu)}>
+                                    Order Now
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-);
+
+    );
+};
 
 export default MenuItem;
