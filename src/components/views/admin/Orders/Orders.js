114f4fa5b16f869b253ad7499fea4a01a8641f6f
Edward Njoroge <codingedward@gmail.com>
2018-07-17 03:39:37 +0300

    Admin add orders

---
+++ src/components/views/admin/Orders/Orders.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+import Filter from 'src/components/common/Filter';
+import Content from 'src/components/common/Content';
+import OrdersTable from './components/OrdersTable';
+import ManageModal from './components/Manage';
+import axios from 'src/axios';
+
+import { singleError, paginationInfo } from 'src/utils';
+
+class Orders extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: 1,
+            data: {},
+            search: '',
+            perPage: 5,
+        }
+    }
+
+    componentWillMount() {
+        this.fetchOrders()
+    }
+
+    fetchOrders = (config = {}) => {
+        let { 
+            page = this.state.page, 
+            perPage = this.state.perPage,
+            search = this.state.search, 
+        } = config;
+        search = (search) ? `name:${search}` : '';
+        const link = 
+            `/orders?related=menu_item|user&page=${page}&search=${search}&per_page=${perPage}&time=today`;
+        this.props.setLoading(true);
+
+        const _this = this;
+        axios.auth();
+        axios.get(link, this.state).then(({ data }) => {
+            const pageInfo = paginationInfo(data);
+            _this.setState({
+                ..._this.state,
+                page: pageInfo.currentPage,
+                data,
+            });
+            _this.props.setLoading(false);
+            if (pageInfo.currentCount === 0 && pageInfo.currentPage !== 1) {
+                _this.fetchOrders({
+                    page: pageInfo.currentPage - 1
+                });
+            }
+        }).catch(({ response }) => {
+            _this.setState({
+                ..._this.state,
+                error: response,
+            })
+            _this.props.setLoading(false);
+        })
+    }
+
+    toggleManage = (e) => {
+        this.setState({
+            ...this.state,
+            manageIsOpen: !this.state.manageIsOpen
+        });
+    }
+
+    onFilter = (text) => {
+        this.setState({
+            ...this.state,
+            search: text,
+        });
+        this.fetchOrders({search: text});
+    }
+
+    onToggle = (order) => {
+        this.setState({
+            ...this.state,
+            manageIsOpen: true,
+            toManage: order,
+        });
+    }
+
+    onPageChange = (page) => {
+        this.setState({
+            ...this.state,
+            page,
+        });
+
+        this.fetchOrders({
+            page: page,
+            search: this.state.search
+        });
+    }
+
+    render() {
+        const { 
+            data,
+            error,
+            pageInfo,
+            toManage,
+            manageIsOpen
+        } = this.state;
+
+
+        const contentTop = (
+            <div className="col-12 mb-2 pr-0 pr-sm-2">
+                <h5 className="d-inline-block">Manage Orders</h5>
+            </div>
+        );
+
+        const contentFilter = (
+            <Filter onFilter={this.onFilter} />
+        );
+
+        return (
+            <Content 
+                {...this.props}
+                contentTop={contentTop} 
+                contentFilter={contentFilter}>
+            
+                { error && <Alert color="danger"> { singleError(error) }</Alert> }
+                     <OrdersTable 
+                         data={data}
+                         pageInfo={pageInfo}
+                         onPrev={this.onPageChange}
+                         onToggle={this.onToggle}
+                         onNext={this.onPageChange} />
+
+                    <ManageModal 
+                        {...this.props}
+                        order={toManage} 
+                        onChange={this.fetchOrders}
+                        isOpen={manageIsOpen} 
+                        toggle={this.toggleManage}/>
+            </Content>
+        );
+    }
+}
+
+export default Orders;
---
+++ src/components/views/admin/Orders/components/Manage/Manage.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import axios from 'src/axios';
+import Modal from 'src/components/common/Modal';
+import { Alert, Button } from 'reactstrap';
+import { ToggleState } from 'src/constants';
+import { singleError } from 'src/utils';
+import './styles.css';
+
+
+class DeleteModal extends React.Component {
+
+    state = {}
+
+    onOpened = () => {
+        this.setState({
+            ...this.state,
+            error: null,
+        });
+    }
+
+    onSetToggleState = (status) => {
+        let { order } = this.props;
+        console.log(order);
+        let newOrder = {
+            user_id: order.user_id,
+            menu_item_id: order.menu_item_id,
+            status,
+        }
+        this.props.setLoading(true);
+        const _this = this;
+        axios.put(`/orders/${order.id}`, newOrder).then(() => {
+            _this.props.onChange();
+            _this.props.toggle();
+            _this.props.setLoading(false);
+        }).catch(({ response }) => {
+            _this.setState({
+                ..._this.state,
+                error: response,
+            });
+            _this.props.setLoading(false);
+        });
+    }
+
+    render() {
+        const { error } = this.state;
+        const { order = {} } = this.props;
+        const body = (
+            <div>
+                {error &&
+                    <Alert className="text-center text-small" color="danger">
+                        { singleError(error) }
+                    </Alert>
+                }
+                <p className="text-center">
+                    Accept or reject this order(ID: <b>{order.id}</b>)
+                </p>
+            </div>
+        );
+
+        const footer = (
+            <div className="m-auto">
+                <Button 
+                    onClick={() => this.onSetToggleState(ToggleState.REJECTED)} 
+                    className="mr-2" 
+                    color="danger">
+                    Reject
+                </Button>
+                <Button 
+                    onClick={() => this.onSetToggleState(ToggleState.ACCEPTED)}  
+                    color="primary">
+                    Accept
+                </Button>
+            </div>
+        );
+
+        const { isOpen, toggle } = this.props;
+        return (
+            <Modal 
+                title="Manage Order Status" 
+                body={body} 
+                footer={footer} 
+                isOpen={isOpen}
+                toggle={toggle}
+                onOpened={this.onOpened}
+            />
+        );
+    }
+}
+
+export default DeleteModal;
