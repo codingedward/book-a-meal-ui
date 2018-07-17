2a295158dcfaf1f231f988c3d7db6018a84ae6f6
Edward Njoroge <codingedward@gmail.com>
2018-07-17 09:32:12 +0300

    Add user menu page filters

--- src/components/views/user/Menu/Menu.js
+++ src/components/views/user/Menu/Menu.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'src/axios';
-import Filter from 'src/components/common/Filter';
 import Content from 'src/components/common/Content';
 import MealMenu from 'src/components/common/Menu';
 import OrderModal from './components/Order';
@@ -8,7 +7,8 @@ import OrderModal from './components/Order';
 class Menu extends React.Component {
 
     state = {
-        data: {}
+        data: {},
+        currentMenu: 0,
     }
 
     componentWillMount() {
@@ -45,7 +45,31 @@ class Menu extends React.Component {
         });
     }
 
+    onFilter = (e) => {
+        this.setState({
+            ...this.state,
+            currentMenu: Number(e.target.value)
+        });
+    }
+
+    onReset = () => {
+        this.setState({
+            ...this.state,
+            currentMenu: 0
+        })
+    }
+
     render() {
+        let { menus = [] } = this.state.data;
+        const { toOrder, orderMenu, orderIsOpen, currentMenu } = this.state;
+
+        let filtered = menus.filter((menu) => {
+            if (currentMenu === 0) {
+                return true;
+            }
+            return menu.id === currentMenu;
+        });
+
         const contentTop = (
             <div className="col-12 mb-2 pr-0 pr-sm-2">
                 <h5 className="d-inline-block">Today's Menu</h5>
@@ -53,17 +77,32 @@ class Menu extends React.Component {
         );
 
         const contentFilter = (
-            <Filter onFilter={this.onFilter} />
+            <div className="row mb-3">
+                <div className="col-3 col-sm-2 col-lg-1 text-right">
+                    <label className="pt-2">Menu:</label>
+                </div>
+                <div className="col-9 col-sm-7 col-lg-4 col-xl-3 pl-0 pr-1">
+                    <select onChange={this.onFilter} className="form-control">
+                        <option value="0">-- All --</option>
+                        {menus.map((menu) => (
+                            <option key={menu.id} value={menu.id}>{menu.name}</option>
+                        ))}
+                    </select>
+                </div>
+                {currentMenu !== 0 &&
+                    <div className="animated fadeIn btn-reset col-12 col-sm-1 col-lg-1 pt-3 pt-sm-0 pl-0">
+                        <button onClick={this.onReset} className="btn btn-secondary d-block m-auto">Reset </button>
+                    </div>
+                }
+            </div>
         );
 
-        const { menus = [] } = this.state.data;
-        const { toOrder, orderMenu, orderIsOpen } = this.state;
         return (
             <Content 
                 {...this.props}
                 contentTop={contentTop} 
                 contentFilter={contentFilter}>
-                {menus.map((menu) => 
+                {filtered.map((menu) => 
                     <MealMenu 
                         menu={menu}
                         key={menu.id} 
