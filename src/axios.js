683d187dd902b827e673ca3e4c13250c8a44ac6f
Edward Njoroge <codingedward@gmail.com>
2018-07-18 00:18:05 +0300

    Fix access-control-allow-origin

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,7 @@ import { BASE_URL } from './constants';
 
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 axios.auth = () => {
     axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
