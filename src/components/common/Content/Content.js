e64045cbd21982c089996570ba21496e152f09d1
Edward Njoroge <codingedward@gmail.com>
2018-07-17 06:17:32 +0300

    Add on click event for humburger menu

--- src/components/common/Content/Content.js
+++ src/components/common/Content/Content.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import menuImg from './assets/menu.png';
 
-const Content = ({ children, contentTop, contentFilter }) => (
+const Content = ({ children, contentTop, contentFilter, toggleMenu }) => (
     <div className="col-12 col-md-8 offset-md-4 col-lg-9 offset-lg-3 container-fluid p-4">
         <div className="row">
             <div className="col-12">
-                <img className="d-md-none controls-icon pb-3" 
-                     alt="Menu"
-                     src={menuImg}/>
+                <img className="d-md-none controls-icon pb-3"  
+                    onClick={toggleMenu}
+                    alt="Menu"
+                    src={menuImg}/>
             </div>
             {contentTop}
         </div>
