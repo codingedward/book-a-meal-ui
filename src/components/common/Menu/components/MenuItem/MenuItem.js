5085851cb64a3a3ca596dc6e5750d3a2d00ed6c6
Edward Njoroge <codingedward@gmail.com>
2018-09-10 00:59:05 +0300

    Fix image not showing on food menu

--- src/components/common/Menu/components/MenuItem/MenuItem.js
+++ src/components/common/Menu/components/MenuItem/MenuItem.js
@@ -6,7 +6,7 @@ import placeholder from './assets/placeholder.png';
 const MenuItem = ({  menu, item, onOrder }) => (
     <div className="col-10 offset-1 offset-sm-0 col-sm-6 col-md-4 col-lg-3 text-center">
         <div className="menu-item pt-3 pl-3 pr-3 m-1">
-            <img src={((!item.img_url) || item.img_url === '#') ? placeholder : item.img_url} className="img-fluid" alt="Food"/>
+            <img src={(!item.meal.img_url || item.meal.img_url === '#') ? placeholder : item.meal.img_url} className="img-fluid" alt="Food"/>
             <table className="table">
                 <tbody>
                     <tr>
